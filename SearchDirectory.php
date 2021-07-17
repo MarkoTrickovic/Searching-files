205ef8413136e4628d933da0b508929231c5277d
Marko Trickovic <trickovic993@gmail.com>
2021-07-17 16:25:23 +0200

    Working on another implementation of method searchDirectoryIteratively

--- SearchDirectory.php
+++ SearchDirectory.php
@@ -3,16 +3,76 @@
 
 class SearchDirectory
 {
-    private $dir;
-    private $fileName;
+    private string $dir;
+    private string $fileName;
 
-    public function __construct($DirName, $FileName) {
-        $dir = $DirName;
-        $fileName = $FileName;
+    public function __construct(string $DirName, string $FileName) {
+        $this->dir = $DirName;
+        $this->fileName = $FileName;
     }
 
-    public function searchDirRecursevly() {
+    public function getDirName() : string {
+        return $this->dir;
+    }
+
+    public function getFileName() : string {
+        return $this->fileName;
+    }
+
+    public function searchDirIteratively() : string {
         $ret = "";
+        if ($handle = opendir($this->dir)) {    // Open dir
+            while (($entry = readdir($handle)) !== false) { // Get one entry
+                if (is_dir($entry) === true) {
+                    // 1. Do scanning inside this directory
+                    // 2. Compare each file with the file that we're looking
+                    $ndirpath = $this->dir . "\\" . $entry;
+                    if ($h2 = opendir($ndirpath)) {
+                        while (($nentry = readdir($h2)) !== false) {
+                            if (is_file($nentry) === true) {
+                                if (strcmp($this->fileName, $nentry) === 0) {
+                                    $ret = $ndirpath . "\\" . $nentry;
+                                    break;
+                                }
+                            }
+                        }
+                        closedir($h2);
+                    }
+                    // 3. Break the loop and return directory
+                } else {
+                    // 1. Compare if it is correct file
+                    // 2. If true, break the loop
+                    if (strcmp($entry, $this->fileName) === 0) {
+                        $ret = $this->dir . "\\" . $entry;
+                        break;
+                    }
+                    // 3. Else, if not, continue
+                }
+                if (strlen($ret) > 0) {
+                    break;
+                }
+            }
+            closedir($handle);
+        }
         return $ret;
     }
+
+    /*
+     * Another, possible solution, when using iterative file listing
+     * is to use the scandir() method, to aquire array with the entr-
+     * ies from the path supplied. Afterwards, searching into the array
+     * can be done easily (or something else can be done with entries).
+     */
+    /**
+     * @param $path - Destination where to look for the file.
+     * @param $filename - Filename which we are looking to find.
+     * @return string - Full destination to the location where file is found. Empty if not found.
+     */
+    public static function searchDirectoryIteratively($path, $filename) : string {
+        $ret = "";
+        $entries = scandir($path);
+
+        return $ret;
+    }
+
 }
--- TestSearch.php
+++ TestSearch.php
@@ -1,20 +1,58 @@
 <?php declare(strict_types=1);
-
-
 use PHPUnit\Framework\TestCase;
 require '/wamp64/www/Searching files/SearchDirectory.php';
 
 class TestSearch extends TestCase
 {
-    public function testSearchDirRecursevly() {
+    public function testSearchDirIteratively() : void {
         $fn = "Test.php";
-        $dp = "C:\\";
+        $dp = "C:\\wamp64\\www";
         $s = new SearchDirectory($dp, $fn);
-        $f = $s->searchDirRecursevly();
-        if (strcmp($f, $fn) !== 0) {
-            $this->assertFileDoesNotExist("$dp + $fn");
+        $f = $s->searchDirIteratively();
+
+        if (strcmp($fn, $f)) {
+            $this->assertSame($fn, $f);
+        }
+
+        if (strlen($f) == 0) {
+            $this->assertFileDoesNotExist($fn, $f);
         } else {
-            $this->assertFileExists($fn);
+            $this->assertFileExists($f);
+        }
+    }
+
+    public function testSearchDirectoryIteratively() : void {
+        $fr = searchDirectoryIteratively("C:\\wamp64\\www", "Test.php");
+        if (strlen($fr) === 0) { // File is not found
+            $this->assertFileDoesNotExist("Test.php", $fr);
+        } else {
+            $this->assertPrint($fr);
+        }
+    }
+
+    public function testDirName() : void {
+        $fn = "Test.php";
+        $dp = "C:\\wamp64\\www";
+        $s = new SearchDirectory($dp, $fn);
+        $d = $s->getDirName();
+
+        if (strlen($d) > 0) {
+            $this->assertSame($dp, $d);
+        } else {
+            $this->assertEmpty($dp);
+        }
+    }
+
+    public function testFileName() : void {
+        $fn = "Test.php";
+        $dp = "C:\\wamp64\\www";
+        $s = new SearchDirectory($dp, $fn);
+        $f = $s->getFileName();
+
+        if (strlen($f) > 0) {
+            $this->assertSame($fn, $f);
+        } else {
+            $this->assertEmpty($fn);
         }
     }
 }
